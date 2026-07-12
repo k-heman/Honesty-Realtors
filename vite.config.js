9025481ef03161eaea65a550a44df0c2d7e009ba
Sunny <k.heman0123@gmail.com>
2026-07-12 09:25:12 +0530

    production level automation

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: true,
-    proxy: {
-      '/api/webhook': {
-        target: 'https://workflow.ccbp.in',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/webhook/, '/webhook-test/a7dd7e3a-8f7e-4af9-8913-10518c362f2f')
-      }
-    }
+    allowedHosts: true
   }
 })
